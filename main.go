3c102e5b5327691f1d2b1096317d659da6ca9362
caili <545939084@qq.com>
2021-07-19 15:56:43 +0800

    '测试上传代码'

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+func main() {
+	router := gin.Default()
+	router.GET("/index", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "hello world",
+		})
+	})
+
+	router.Run(":8080")
 }
